0efab7d7a9744972ad88d5e1200b9403bba2231f
Həsən <hesenelnuroglu07@gmail.com>
2026-02-09 00:22:51 +0400

    Update app.py

--- python-scripts/app.py
+++ python-scripts/app.py
@@ -1,12 +1,1 @@
-from flask import Flask, render_template
-from python_scripts.scanner import scan_results
 
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    results = scan_results()
-    return render_template("index.html", results=results)
-
-if __name__ == "__main__":
-    app.run(debug=True)
